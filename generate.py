51e1958910aabbb5ee6553928bd2fb2da128b924
OlinRB <olinrb@live.com>
2022-01-29 16:45:51 -0500

    Many Links done

--- generate.py
+++ generate.py
@@ -1,12 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-length = 1
-width = 1
-height = 1
+pyrosim.Start_SDF("boxs.sdf")
+
 x = 0
 y = 0
-z = 0
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+z = .5
+for k in range(5):
+    for j in range(5):
+        length = 1
+        width = 1
+        height = 1
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x+k,y+j,z+i] , size=[length,width,height])
+            width = width * .9
+            length = length * .9
+            height = height * .9
+
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
 import time
 import pybullet_data
 
+#Single Link -> https://imgur.com/3eGoPiT
+#Many links -> https://imgur.com/gallery/sml7fZC
+
 #test
 import pybullet as p
 
@@ -10,8 +13,10 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeID = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxs.sdf")
+i = 0
 while run:
+    time.sleep(1/60)
     p.stepSimulation()
 p.disconnect()
 
