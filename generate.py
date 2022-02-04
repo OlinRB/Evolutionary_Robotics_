08a4ceeebb38bcc07250227ae9399cfa16242d50
OlinRB <olinrb@live.com>
2022-02-03 23:15:58 -0500

    Sensor only outputs when hit with other block

--- generate.py
+++ generate.py
@@ -11,16 +11,16 @@ height = 1
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x-2, y-2, z], size=[length, width, height])
+    pyrosim.Send_Cube(name="T", pos=[x-2, y-2, z], size=[length, width, height])
     pyrosim.End()
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="BackLeg", pos=[x, y, z], size=[length, width, height])
-    pyrosim.Send_Joint(name="BackLeg-Torso", parent="BackLeg", child="Torso",
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="BackLeg", child="Torso",
                        type="revolute", position=[1, 0, 1])
     pyrosim.Send_Cube(name="Torso", pos=[.5, .5, .5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Torso-FrontLeg", parent="Torso", child="FrontLeg",
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg",
                        type="revolute", position=[1,.5,0])
     pyrosim.Send_Cube(name="FrontLeg", pos=[.5, 0, -.5], size=[length, width, height])
 
--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import time
 import pybullet_data
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 
 #Sensors
 run = True
@@ -8,12 +9,18 @@ run = True
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
+
 planeID = p.loadURDF("plane.urdf")
 robotID = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 i = 0
+
+pyrosim.Prepare_To_Simulate(robotID)
 while run:
-    time.sleep(1/60)
+
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
+    time.sleep(1 / 60)
 p.disconnect()
 
