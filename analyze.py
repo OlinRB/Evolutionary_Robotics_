5079ef9d29bd69c1734269797e1d5897742e223d
OlinRB <olinrb@live.com>
2022-02-11 23:01:27 -0500

    Motors complete

--- analyze.py
+++ analyze.py
@@ -7,11 +7,14 @@ with open("data\\sensor_values.npy", "rb") as file:
 with open("data\\frontLegSensorValues.npy", "rb") as file:
     front_values = np.load(file)
 
-with open("data\\targetValues.npy", "rb") as file:
-    target_values = np.load(file)
+with open("data\\targetValues_BackLeg.npy", "rb") as file:
+    target_values_BackLeg = np.load(file)
 
-print(target_values)
-plot.plot(target_values, label="Target Values")
+with open("data\\targetValues_FrontLeg.npy", "rb") as file:
+    target_values_FrontLeg = np.load(file)
+
+plot.plot(target_values_BackLeg, label="Back Leg Angles", linewidth=3)
+plot.plot(target_values_FrontLeg, label="Front Leg Angles")
 # plot.plot(back_values, label="Back Leg Sensor Values", linewidth=5)
 # plot.plot(front_values, label="Front Leg Sensor Values")
 plot.legend()
--- simulate.py
+++ simulate.py
@@ -20,23 +20,31 @@ robotID = p.loadURDF("body.urdf")
 #p.loadSDF("world.sdf")
 i = 0
 pi = 3.1415926536
-motor_val = 25
-amplitude = np.pi/4
-frequency = 1
-phaseOffset = 0
+motor_val = 15
+
+amplitude_BackLeg = np.pi/4
+frequency_BackLeg = 8
+phaseOffset_BackLeg = 5
+amplitude_FrontLeg = np.pi/4
+frequency_FrontLeg = 125
+phaseOffset_FrontLeg = 1* (np.pi/4)
 
 file1 = "data\\sensor_values.npy"
 file2 = "data\\frontLegSensorValues.npy"
-file3 = "data\\targetValues.npy"
+file3 = "data\\targetValues_BackLeg.npy"
+file4 = "data\\targetValues_FrontLeg.npy"
 pyrosim.Prepare_To_Simulate(robotID)
 backLegSensorValues = numpy.zeros(LOOP_LEN)
 frontLegSensorValues = numpy.zeros(LOOP_LEN)
-targetAngles = np.linspace(0, 2*np.pi, LOOP_LEN)
+targetAngles_BackLeg = np.linspace(0, 2*np.pi, LOOP_LEN)
+targetAngles_FrontLeg = np.linspace(0, 2*np.pi, LOOP_LEN)
 for i in range(LOOP_LEN):
     # targetAngles[i] = targetAngles[i] * (pi / 4)
-    targetAngles[i] = amplitude * np.sin(frequency * targetAngles[i] + phaseOffset)
-np.save(file3, targetAngles)
-exit()
+    targetAngles_BackLeg[i] = amplitude_BackLeg * np.sin(frequency_BackLeg * targetAngles_BackLeg[i] + phaseOffset_BackLeg)
+    targetAngles_FrontLeg[i] = amplitude_FrontLeg * np.sin(frequency_FrontLeg * targetAngles_FrontLeg[i] + phaseOffset_FrontLeg)
+# np.save(file3, targetAngles_BackLeg)
+# np.save(file4, targetAngles_FrontLeg)
+# exit()
 for i in range(LOOP_LEN):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
@@ -45,13 +53,13 @@ for i in range(LOOP_LEN):
         bodyIndex=robotID,
         jointName="BackLeg_Torso",
         controlMode=p.POSITION_CONTROL,
-        targetPosition=targetAngles[i],
+        targetPosition=targetAngles_BackLeg[i],
         maxForce=motor_val)
     pyrosim.Set_Motor_For_Joint(
         bodyIndex=robotID,
         jointName="Torso_FrontLeg",
         controlMode=p.POSITION_CONTROL,
-        targetPosition=-targetAngles[i],
+        targetPosition=-targetAngles_FrontLeg[i],
         maxForce=motor_val)
     time.sleep(1 / 60)
 p.disconnect()
